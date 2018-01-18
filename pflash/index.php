f20da817116e2e80dc91f9dab52650588d9cadfe
Gabriel Ochsenhofer <gabs@gabs.tv>
2018-01-18 18:32:44 -0200

    old site

---
+++ pflash/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Gabs - The portfolio of Gabriel Ochsenhofer</title>
+<script src="js/swfobject.js" type="text/javascript"></script>
+<script type="text/javascript">
+swfobject.embedSWF("portfolio_v12.swf", "myContent", "100%", "100%", "10.0.0", "expressInstall.swf");
+</script>
+<style type="text/css">
+/* hide from ie5 mac \*/
+html {
+  height: 100%;
+  overflow: hidden;
+}
+#myContent
+{
+	height:100%;
+}
+/* end hide */
+body
+{
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #000;
+}
+</style>
+</head>
+
+<body>
+<div id="myContent">
+    <h1>Get Flash</h1>
+    <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /></a></p>
+</div>
+</body>
+</html>
